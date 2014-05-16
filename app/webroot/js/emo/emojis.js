12f21581dfb5f118724dceb05e22ade45777c9cb
Ankan Adhikari <feraasd@gmail.com>
2014-05-16 22:14:55 +0530

    all js/css deleted, classroom and ajax all css and js was deleted to start from scratch owing to unmanagability. some untested layout code.

--- app/webroot/js/emo/emojis.js
+++
@@ -1,8 +0,0 @@
-$.emojiarea.path = 'js/emo';
-$.emojiarea.icons = {
-	':smile:'     : 'smile.png',
-	':angry:'     : 'angry.png',
-	':flushed:'   : 'flushed.png',
-	':neckbeard:' : 'neckbeard.png',
-	':laughing:'  : 'laughing.png'
-};
