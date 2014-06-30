e0e12498d209b02330800419c92bbde52bf73d48
Ankan Adhikari <feraasd@gmail.com>
2014-07-01 05:13:17 +0530

    lame for-loop undeffered email takes a serious amount of time to dispatch emails :D

--- app/Controller/AnnouncementsController.php
+++ app/Controller/AnnouncementsController.php
@@ -1,5 +1,6 @@
 <?php
 
+App::uses('CakeEmail', 'Network/Email');
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -70,6 +71,7 @@ class AnnouncementsController extends AppController {
             $data = $this->Announcement->getAnnouncementById($this->Announcement->getLastInsertID());
             $message = "Announcement created";
             $status = true;
+            $this->tasty($classroomId, $data);
         } else {
             $message = "Could not create announcement";
             $status = false;
@@ -104,4 +106,41 @@ class AnnouncementsController extends AppController {
 //            $this->set('_serialize', array('status', 'message'));
 //        }
 //    }
+
+    public function tasty($classroomId, $announcement) {
+
+        /**
+         * Get all the users' emails
+         */
+        $options['contain'] = array(
+            'Classroom' => array(
+                'fields' => array('id')
+            ),
+            'AppUser' => array(
+                'fields' => array('id', 'email')
+            )
+        );
+
+        $options['conditions'] = array(
+            'UsersClassroom.classroom_id' => $classroomId
+        );
+        $data = $this->Announcement->Classroom->UsersClassroom->find('all', $options);
+        $emails = Hash::extract($data, '{n}.AppUser.email');
+
+        /**
+         * loop and send emails
+         * TODO: defer emails
+         */
+        $Email = new CakeEmail();
+        $Email->config('mailgun');
+        foreach ($emails as $email) {
+            $Email->from(array('announcements@pyoopil.com' => 'Pyoopil Announcements'));
+            $Email->to($email);
+            $Email->subject($announcement['Announcement']['subject']);
+            $body = $announcement['Announcement']['body'] . PHP_EOL . PHP_EOL . Router::url('/', true) . 'Classrooms/' . $classroomId . '/Announcements';
+            $Email->send($body);
+        }
+//        $this->log($body);
+    }
+
 }
