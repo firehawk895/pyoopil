4b7241e0e7d693bd496072db77869ffd03c8193e
naveen gupta <naveen@greeenapplesolutions.com>
2014-08-13 16:24:09 +0530

    working on classroom page

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -9,25 +9,42 @@
  * Main module of the application.
  */
 angular
-  .module('uiApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('uiApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ui.router',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/rooms");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('rooms', {
+                url: "/rooms",
+                templateUrl: "views/rooms/index.html"
+            });
+//            .state('rooms.', {
+//                url: "/list",
+//                templateUrl: "partials/state1.list.html",
+//                controller: function ($scope) {
+//                    $scope.items = ["A", "List", "Of", "Items"];
+//                }
+//            })
+//            .state('state2', {
+//                url: "/state2",
+//                templateUrl: "partials/state2.html"
+//            })
+//            .state('state2.list', {
+//                url: "/list",
+//                templateUrl: "partials/state2.list.html",
+//                controller: function ($scope) {
+//                    $scope.things = ["A", "Set", "Of", "Things"];
+//                }
+//            })
+
+
+    }]);
