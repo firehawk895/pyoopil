05f05b8705561fe1c0cfe8c7245c9ceb4c9c7e43
hm6293 <hmittal@greenapplesolutions.com>
2014-08-19 12:45:03 +0530

    create and display announcements done functionality to create announcement if permitted

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -9,47 +9,53 @@
  * Main module of the application.
  */
 angular
-  .module('uiApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ui.router',
-    'ngSanitize',
-    'restangular',
-    'LocalStorageModule',
-    'ngTouch'
-  ])
-  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    .module('uiApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ui.router',
+        'ngSanitize',
+        'restangular',
+        'LocalStorageModule',
+        'ngTouch',
+        'infinite-scroll'
+    ])
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
-    $locationProvider.html5Mode(true);
-    $urlRouterProvider.otherwise("/rooms");
-    //
-    // Now set up the states
-    $stateProvider
-      .state('rooms', {
-        url: "/rooms/",
-        templateUrl: "views/rooms/index.html"
-      })
-      .state('login', {
-        url: "/login/",
-        templateUrl: "views/home/login.html",
-        controller: "LoginCtrl"
-      });
-  }])
-  .controller('MainController', ['$scope', 'globalService', function ($scope, globalService) {
-    $scope.isLoggedIn = globalService.getIsAuthorised();
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise("/rooms/");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('rooms', {
+                url: "/rooms/",
+                templateUrl: "views/rooms/index.html"
+            })
+            .state('login', {
+                url: "/login/",
+                templateUrl: "views/home/login.html",
+                controller: "LoginCtrl"
+            })
+            .state('announcements', {
+                url: "/rooms/:roomId/announcements/",
+                templateUrl: "views/rooms/announcement.html",
+                controller: "announcementCtrl"
+            });
+    }])
+    .controller('MainController', ['$scope', 'globalService', function ($scope, globalService) {
+        $scope.isLoggedIn = globalService.getIsAuthorised();
 
-  }])
-  //main run module for the whole application
-  .run(['$http', 'globalService', 'Restangular',
-    function ($http, globalService, restangular) {
+    }])
+    //main run module for the whole application
+    .run(['$http', 'globalService', 'Restangular',
+        function ($http, globalService, restangular) {
 
-      //set base path for restangular
-      restangular.setBaseUrl(globalService.getBaseUrl());
+            //set base path for restangular
+            restangular.setBaseUrl(globalService.getBaseUrl());
 
-      $http.defaults.headers.common = {'X-AuthTokenHeader': '123456'};
+            $http.defaults.headers.common = {'X-AuthTokenHeader': '53f2f038-e8d4-47d3-a681-03f90130a31c'};
 
 //      userService.validateSession();
 //      $idle.watch();
-    }])
+        }])
 ;
--- ui/app/scripts/controllers/rooms/announcement.js
+++ ui/app/scripts/controllers/rooms/announcement.js
@@ -8,10 +8,35 @@
  * Controller of the uiApp
  */
 angular.module('uiApp')
-  .controller('AnnouncementCtrl', function ($scope) {
+    .controller('announcementCtrl', ['$scope', '$stateParams' , 'roomService', 'notificationService', function ($scope, $stateParams, roomService, notificationService) {
+        $scope.announcement = {};
+        //todo : check if room id has access
+        $scope.page = 1;
+        roomService.getAnnouncements($stateParams.roomId, $scope.page).then(function (result) {
+            $scope.announcements = result.data;
+            $scope.canPost=result.permissions.allowCreate;
+        });
 
+        $scope.createAnnouncement = function () {
 
+            roomService.createAnnouncement($stateParams.roomId, $scope.announcement).then(function (added) {
+                notificationService.show(added.status, added.message);
+                $scope.announcements.unshift(added.data);
+                $scope.announcement = {};
+            });
+        };
 
+        $scope.myPagingFunction = function () {
+            roomService.getAnnouncements($stateParams.roomId, ++$scope.page).then(function (result) {
+                $scope.announcements = $scope.announcements.concat(result.data);
+            });
+        };
+//        $scope.subCharLeft = function () {
+//            var charLeft = 200 - $scope.announcement.Announcement.subject.length;
+//            if (charLeft==undefined)
+//                return 0;
+//
+//            return charLeft;
+//        };
 
-
-  });
+    }]);
