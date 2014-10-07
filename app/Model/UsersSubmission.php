4cff5b794866cc64b0d3f8189ecf029b993127a0
Ankan Adhikari <feraasd@gmail.com>
2014-10-07 17:46:01 +0530

    stubbed out method createDummyUsersSubmissions()

--- app/Model/UsersSubmission.php
+++ app/Model/UsersSubmission.php
@@ -159,4 +159,44 @@ class UsersSubmission extends AppModel {
         return $this->find('all', $options);
     }
 
+    /**
+     * creating entries for student submissions
+     * Use cases:
+     * 1. Submission due date has passed and some users have not submitted anything
+     * 2. Create Report of an on offline submission requires usersSubmissions to be published
+     * This must exclude the owner/educator
+     * @param $submissionId
+     * @return bool
+     */
+    public function createDummyUsersSubmissions($submissionId) {
+        //get parent classroom of this submission
+        //get all users of that classroom excluding educator, who have not submitted :S
+        //make users_submission entries for all of these
+        //set is_submitted for all of this
+        $this->Submission->id = $submissionId;
+        $classroomId = $this->Submission->field('classroom_id');
+    }
+
+    /**
+     *
+     * @param $classroomId
+     */
+    private function _getFilteredPeople($classroomId) {
+//        $usersSubmission = new UsersSubmission();
+//
+//        $options = array(
+//            'contains' => array(
+//                'AppUser' => array(
+//                    'id'
+//                )
+//            ),
+//            'conditions' => array(
+//                'classroom_id' => $classroomId,
+//            ),
+//            'fields' => array(
+//                'id'
+//            )
+//        );
+    }
+
 }
