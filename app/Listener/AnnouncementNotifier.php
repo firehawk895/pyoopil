f7b32d77f45c3f3987451c76dada43b65a1e73f9
Nakul Gulati <nakgulati@gmail.com>
2014-10-14 03:57:11 +0530

    Change in notification array

--- app/Listener/AnnouncementNotifier.php
+++ app/Listener/AnnouncementNotifier.php
@@ -18,11 +18,41 @@ class AnnouncementNotifier implements CakeEventListener{
 
         $studentList = $this->UsersClassroom->getStudentList($event->data['classroomId']);
 
+        $userIdList = Hash::extract($studentList,'{n}.AppUser.id');
+
         $announcement = $this->Announcement->getAnnouncementById($event->data['announcementId']);
         $classroomTitle = $this->UsersClassroom->Classroom->getClassroomTitle($event->data['classroomId']);
 
-        $notification = "Announcement in classroom: {$classroomTitle}"." {$announcement['Announcement']['subject']}";
+        $notification = array(
+            'title' => "Announcement in classroom: {$classroomTitle}"." {$announcement['Announcement']['subject']}",
+            'is_read' => false,
+            'link' => "Classroom/{$event->data['classroomId']}/announcements",
+            'is_clicked' => false,
+            'created' => $announcement['Announcement']['created']
+        );
 
+        //firebase stuff
+        //get object
+        //push
+        //set
+
+    }
+
+    private function testNotification() {
+        $firebase = new Firebase('https://vivid-torch-3610.firebaseio.com/', 'p0TcCv6cmjUKhbBBJb0tyCjJCVxAzKuakDJLxjGy');
+        $announcement = $this->Announcement->getAnnouncementById($this->Announcement->getLastInsertID());
+        $notification = array(
+            'title' => " new announcement has been posted by <strong>Aaron Bmoit</strong> in <strong>Debating</strong>",
+            'is_read' => false,
+            'link' => "Classroom/36/announcements",
+            'is_clicked' => false,
+            'created' => $announcement['Announcement']['created']
+        );
+        $path = AuthComponent::user('id') . '/nof/';
+        $firebase->push($path, $notification);
+
+        $path = AuthComponent::user('id') . '/unread';
+        $firebase->set($path, '3');
     }
 
 } 
