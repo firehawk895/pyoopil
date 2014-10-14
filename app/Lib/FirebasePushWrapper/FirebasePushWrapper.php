ad0d97b18261a977275b78df2766d22138cd3d80
Nakul Gulati <nakgulati@gmail.com>
2014-10-14 06:56:10 +0530

    Push implementation - crude :/

--- app/Lib/FirebasePushWrapper/FirebasePushWrapper.php
+++ app/Lib/FirebasePushWrapper/FirebasePushWrapper.php
@@ -12,6 +12,21 @@ class FirebasePushWrapper implements NotifyableInterface {
         //for loop over objects and do a push
         //or perhaps there should have been a bulk push
         //or make your own parallizable implementation
+
+        //find a better place to put it
+        $firebase = new Firebase('https://vivid-torch-3610.firebaseio.com/', 'p0TcCv6cmjUKhbBBJb0tyCjJCVxAzKuakDJLxjGy');
+
+        foreach($usersList as $id){
+
+            $path = $id . '/nof/';
+            $firebase->push($path, $message);
+
+            $path = $id . '/unread';
+            $unread = $firebase->get($path);
+            $unread += 1;
+
+            $firebase->set($path, $unread);
+        }
     }
 
 }
